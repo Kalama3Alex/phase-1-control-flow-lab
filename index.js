040bd5021975a2380d681559bfee01f55743c241
Kalama3Alex <alexkalama58@gmail.com>
2022-11-25 21:52:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+    let result;
+    if (ride <=400){
+      result = 'This one is on me!'
+    }
+    else if (ride > 2000 && ride <2500){
+      result = 'I will gladly take your thirty bucks.'
+    }
+    else{
+      result = 'No can do.'}
+      return result;
+    
+    }
 }
+  
+  // Write your code here!
+
 
 function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    
+    return city==="NYC" ? "Ok, sounds good." : "No go.";
+  }
 }
+  // Write your code here!
+
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case "generous":
+        return "Thank you so much.";
+        break;
+      default:
+        return "Bye.";
+    }
+  }
 }
+  // Write your code here!
